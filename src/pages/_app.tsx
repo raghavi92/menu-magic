f583d01295fc0c0983a07634ad520a4aef294c2c
Raghavi Kannan <raghavi.kannan@thoughtworks.com>
2023-08-03 00:08:49 +0530

    Sign in with google button - wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Script src="https://accounts.google.com/gsi/client" async></Script>
+  <Component {...pageProps} />
+  </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import { type } from 'os';
 import { useEffect, useState } from 'react'
-import supabase from '../supabase/supabase';
 import { GetStaticProps, NextPage, InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 type Dish = {
   id: string;
@@ -11,7 +11,17 @@ type Dish = {
 }
 
 
-const Home : NextPage  = () => {
+const Home: NextPage = () => {
+  const supabase = createClientComponentClient()
+  if (typeof window !== "undefined") {
+    window.handleSignin = async (googleUser: any) => {
+      const { data, error } = await supabase.auth.signInWithIdToken({
+        provider: 'google',
+        token: googleUser.credential,
+      })
+    }
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
@@ -29,8 +39,27 @@ const Home : NextPage  = () => {
           Menu Magic is a meal planning app that helps you plan your meals for
           the week.
         </p>
+      </div>
+      <div className="center m-24">
+        <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8 mr-8" href="/explore">Explore</Link>
+        <div id="g_id_onload"
+          data-client_id={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+          data-context="signin"
+          data-ux_mode="popup"
+          data-callback="handleSignin"
+          data-nonce=""
+          data-auto_prompt="false">
         </div>
-        <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8" href="/explore">Explore</Link>
+
+        <div className="g_id_signin"
+          data-type="standard"
+          data-shape="rectangular"
+          data-theme="outline"
+          data-text="signin_with"
+          data-size="large"
+          data-logo_alignment="left">
+        </div>
+      </div>
     </main>
   )
 }
